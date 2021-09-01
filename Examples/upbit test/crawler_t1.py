4abb9839c209c3c81dd3b098b635993f1d33347f
hyemin-Kim <hyemin2060@naver.com>
2021-09-01 14:09:06 +0900

    add upbit test

---
+++ Examples/upbit test/crawler_t1.py
@@ -0,0 +1,48 @@
+import requests
+import os
+
+check_file = os.path.isfile("D:\\4. My github\\webdataconnector\\Examples\\upbit test\\upbit_data_t1.json")
+
+# check_file = "upbit_data_t1.json"
+
+
+
+if check_file is True:
+    file = open("upbit_data_t1.json", 'w')
+if check_file is False:
+    file = open("upbit_data_t1.json", 'w+')
+
+
+def func1():
+    market = {"KRW-ADA", "KRW-BTC", "KRW-AXS", "KRW-DOGE", "KRW-ETC", "KRW-ETH", "KRW-LINK", "KRW-LTC", "KRW-MANA", "KRW-NEO", "KRW-TRX", "KRW-VET", "KRW-XRP"}
+    url = "https://api.upbit.com/v1/candles/days"
+    for i in market:
+        querystring = {"market":i,"count":"20"}
+        headers = {"Accept": "application/json"}
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        file.write(response.text)
+    #file.close()
+
+func1()
+
+files = open("upbit_data_t1.json", 'r')
+lines = files.readlines()
+string = "Too many"
+
+for lines in lines:
+    if string in lines:
+        func1()
+    else:
+        print("Success")
+        break
+
+start = open("upbit_data_t1.json", "r", encoding="UTF-8")
+
+for line in start:
+    line = line.strip()
+    changes = line.replace("][", ",")
+start.close()
+
+end = open("upbit_data_t1.json", "w", encoding="UTF-8")
+end.write(changes)
+end.close()
---
+++ Examples/upbit test/create_table_t1.js
@@ -0,0 +1,53 @@
+(function() {
+    // Create the connector object
+    var myConnector = tableau.makeConnector();
+
+    // Define the schema
+    myConnector.getSchema = function(schemaCallback) {
+		var cols = [{
+            id: "market",
+            dataType: tableau.dataTypeEnum.String
+        }, {
+            id: "candle_date_time_kst",
+            alias: "date",
+            dataType: tableau.dataTypeEnum.date
+        }];
+
+        var tableSchema = {
+            id: "upbit",
+            alias: "upbit data test",
+            columns: cols
+        };
+
+        schemaCallback([tableSchema]);
+    };
+
+    // Download the data
+    myConnector.getData = function(table, doneCallback) {
+        $.getJSON("https://raw.githubusercontent.com/BItableau/BItableau.github.io/main/test_loop.json", function(resp) {
+            var feat = resp,
+                tableData = [];
+
+            // Iterate over the JSON object
+            for (var i = 0, len = feat.length; i < len; i++) {
+                tableData.push({
+			"market": feat[i].market,
+            "candle_date_time_kst": feat[i].candle_date_time_kst
+                });
+            }
+
+            table.appendRows(tableData);
+            doneCallback();
+        });
+    };
+
+    tableau.registerConnector(myConnector);
+
+    // Create event listeners for when the user submits the form
+    $(document).ready(function() {
+        $("#submitButton").click(function() {
+            tableau.connectionName = "upbit data test"; // This will be the data source name in Tableau
+            tableau.submit(); // This sends the connector object to Tableau
+        });
+    });
+})();
