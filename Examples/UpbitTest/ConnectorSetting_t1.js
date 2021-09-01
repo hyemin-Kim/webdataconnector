bf17f3765cad7a399bcdb10a9f2e26b1995270ba
hyemin-Kim <64396319+hyemin-Kim@users.noreply.github.com>
2021-09-01 15:09:08 +0900

    Update ConnectorSetting_t1.js

--- Examples/UpbitTest/ConnectorSetting_t1.js
+++ Examples/UpbitTest/ConnectorSetting_t1.js
@@ -5,8 +5,9 @@
     // Define the schema
     myConnector.getSchema = function(schemaCallback) {
 		var cols = [{
-            id: "market",
-            dataType: tableau.dataTypeEnum.String
+            id: "candle_date_time_kst",
+            alias: "date",
+            dataType: tableau.dataTypeEnum.date
         }];
 
         var tableSchema = {
@@ -27,7 +28,7 @@
             // Iterate over the JSON object
             for (var i = 0, len = feat.length; i < len; i++) {
                 tableData.push({
-			"market": feat[i].market
+			"candle_date_time_kst": feat[i].candle_date_time_kst
                 });
             }
 
